6786a62457cad79e68d43564eccf0d04b114740e
Wannabeasmartguy <997139385@qq.com>
2024-07-17 17:31:10 +0800

    Add LLM model configuration and update default parameters

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -2,14 +2,32 @@
 from openai import OpenAI
 from loguru import logger
 
+from typing import List
+
+DEFAULT_MODEL = "qwen2:1.5b"
+DEFAULT_CONFIG = [
+    {
+        "model": DEFAULT_MODEL,
+        "api_key": "noneed",
+        "base_url": "http://localhost:11434/v1",
+        "params": {
+            "temperature": 0.3,
+            "top_p": 0.5,
+            "max_tokens": 2048,
+            "stream": False,
+        },
+    }
+]
 
 class LLM:
     """
     LLM class encapsulates the logic to load a pre-trained language model and generate text based on input prompts.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, config_list: List = DEFAULT_CONFIG) -> None:
         logger.info("Loading model...")
+        self.config_list = config_list
+        self.defult_config = config_list[0]
         self.system_prompt = (
             "You are responsible for rephrasing, summarizing, or editing various text snippets to make them more "
             "concise, coherent, and engaging. You are also responsible for writing emails, messages, and other forms "
@@ -17,13 +35,13 @@ class LLM:
         )
         
         self.llm = OpenAI(
-            base_url = 'http://localhost:11434/v1',
-            api_key='noneed', # required, but unused
+            base_url = self.defult_config["base_url"],
+            api_key = self.defult_config["api_key"], 
         )
         logger.info("Model loaded successfully.")
 
     def generate(
-        self, text: str, max_tokens: int = 2048, temperature: float = 0.3
+        self, text: str
     ) -> str:
         """
         Generate a response from the language model based on the provided text.
@@ -35,13 +53,13 @@ class LLM:
         """
         logger.info(f"Generating response for text: {text}")
         response = self.llm.chat.completions.create(
-            model = "qwen2:latest",
+            model = self.defult_config["model"],
             messages=[
                 {"role": "system", "content": self.system_prompt},
                 {"role": "user", "content": text},
             ],
-            temperature=temperature,
-            max_tokens=max_tokens,
+            temperature=self.defult_config["params"]["temperature"],
+            max_tokens=self.defult_config["params"]["max_tokens"],
         )
         logger.info("response: {}".format(response))
         return response.choices[0].message.content
